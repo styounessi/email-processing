9820570521558be450bd38c5530a4513431b1b6e
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2023-07-27 19:11:19 -0700

    Improved formatting and refactored insert to mongo function

--- processes/insertion.py
+++ processes/insertion.py
@@ -5,8 +5,10 @@ import polars as pl
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 
+
 # Path to parquet files
 PARQUET_FILE = '/opt/airflow/parquet/*.parquet'
+
 
 def read_latest_file():
     '''
@@ -24,6 +26,7 @@ def read_latest_file():
     df = pl.read_parquet(latest_file)
     return df
 
+
 def mongo_collection():
     '''
     Connects to MongoDB instance and returns the "emails"
@@ -32,16 +35,17 @@ def mongo_collection():
     client = MongoClient('mongodb://mongodb:27017/')
     db = client['productDB']
     collection = db['emails']
-    return collection
+    return client, collection
 
-def main():
+
+def process_and_insert_to_mongo():
     '''
-    Main function to read the latest parquet file, create a compound
-    index, and insert new records into "emails" collection while skipping
-    any duplicate records detected.
+    Read the latest parquet file, create a compound index, and insert
+    new records into "emails" collection while skipping any duplicate
+    records detected.
     '''
     df = read_latest_file()
-    collection = mongo_collection()
+    client, collection = mongo_collection()
 
     records = df.to_dicts()
     
@@ -51,20 +55,19 @@ def main():
     collection.create_index([(field, 1) for field in combined_fields],
                             unique=True)
 
-    '''
-    The idea below is to catch existing records from being inserted again. If a 
-    'BulkWriteError' does happen, it will print which record(s) were caught so
-    they can be investigated further.
-    '''
-
+    # Records are inserted and the number of records is printed
     try:
         insertion = collection.insert_many(records, ordered=False)
         num_records = len(insertion.inserted_ids)
+    # If a 'BulkWriteError' does happen, it will print which record(s) were caught
     except BulkWriteError as bwe:
         num_records = bwe.details['nInserted']
         print(bwe.details)
 
     print(f'Total records inserted: {num_records}.')
 
+    client.close()
+
+
 if __name__ == '__main__':
-    main()
+    process_and_insert_to_mongo()
